f3d236798bdc50021fd5680aaabd36cfcb7b7ba3
suchi <suchi.bansal@10i.co.in>
2016-12-02 14:14:38 +0530

    asyntask added

--- app/src/main/java/com/example/gaurav/railslogin/LoginActivity.java
+++ app/src/main/java/com/example/gaurav/railslogin/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.gaurav.railslogin;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -11,6 +14,7 @@ import android.widget.EditText;
 public class LoginActivity extends Activity {
     private EditText email;
     private EditText password;
+    private Button email_sign_in_button;
 
 
     @Override
@@ -23,5 +27,57 @@ public class LoginActivity extends Activity {
     private void initView() {
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        email_sign_in_button=(Button) findViewById(R.id.email_sign_in_button);
+        email_sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stremail=email.getText().toString();
+                String strpwd=password.getText().toString();
+                new AsyncTaskRunner().execute("aa","aa");
+            }
+        });
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            return null;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see android.os.AsyncTask#onPreExecute()
+         */
+        @Override
+        protected void onPreExecute() {
+            // Things to be done before execution of long running operation. For
+            // example showing ProgessDialog
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+         */
+        @Override
+        protected void onProgressUpdate(String... text) {
+            // Things to be done while execution of long running operation is in
+            // progress. For example updating ProgessDialog
+        }
     }
 }
