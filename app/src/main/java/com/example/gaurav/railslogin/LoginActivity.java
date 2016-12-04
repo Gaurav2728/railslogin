2ad6c8391fd0e9a04a1bca7e26513ae4ea4b1377
suchi <suchi.bansal@10i.co.in>
2016-12-04 12:45:21 +0530

    webservice added

--- app/src/main/java/com/example/gaurav/railslogin/LoginActivity.java
+++ app/src/main/java/com/example/gaurav/railslogin/LoginActivity.java
@@ -1,11 +1,28 @@
 package com.example.gaurav.railslogin;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.text.BreakIterator;
+
+import cz.msebera.android.httpclient.Header;
+
+import static android.R.attr.name;
 
 /**
  * Created by suchi on 27/11/16.
@@ -15,7 +32,7 @@ public class LoginActivity extends Activity {
     private EditText email;
     private EditText password;
     private Button email_sign_in_button;
-
+    ProgressDialog pdLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,7 @@ public class LoginActivity extends Activity {
     }
 
     private void initView() {
+        pdLoading = new ProgressDialog(LoginActivity.this);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         email_sign_in_button=(Button) findViewById(R.id.email_sign_in_button);
@@ -33,51 +51,75 @@ public class LoginActivity extends Activity {
             public void onClick(View view) {
                 String stremail=email.getText().toString();
                 String strpwd=password.getText().toString();
-                new AsyncTaskRunner().execute("aa","aa");
+                RequestParams params = new RequestParams();
+                params.put("email", stremail);
+                // Put Http parameter username with value of Email Edit View control
+                params.put("password", strpwd);
+                // Invoke RESTful Web Service with Http parameters
+
+                //right now its get request make it post
+                invokeWS(params);
             }
         });
     }
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        pdLoading.setMessage("Loading...");
+        pdLoading.show();
 
-    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://www.mocky.io/v2/5843c1a5100000ed181a5712",params ,new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                pdLoading.hide();
+                try {
+                  String str = new String(responseBody, "UTF-8");
 
-        private String resp;
+                    Gson gson = new Gson();
 
-        @Override
-        protected String doInBackground(String... params) {
+                    //convert java object to JSON format
+                    LoginData data=gson.fromJson(str,LoginData.class);
+                    if(data!=null)
+                    {
+                        if(data.getStatus().equals("200"))
+                        {
+                            Toast.makeText(getApplicationContext(), "Login successfully", Toast.LENGTH_LONG).show();
+                            //login successfull here call another screen here
 
-            return null;
-        }
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+                          //error occurr
+                        }
+                    }
 
-        /*
-         * (non-Javadoc)
-         *
-         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-         */
-        @Override
-        protected void onPostExecute(String result) {
-            // execution of result of Long time consuming operation
-        }
 
-        /*
-         * (non-Javadoc)
-         *
-         * @see android.os.AsyncTask#onPreExecute()
-         */
-        @Override
-        protected void onPreExecute() {
-            // Things to be done before execution of long running operation. For
-            // example showing ProgessDialog
-        }
+                }  catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
 
-        /*
-         * (non-Javadoc)
-         *
-         * @see android.os.AsyncTask#onProgressUpdate(Progress[])
-         */
-        @Override
-        protected void onProgressUpdate(String... text) {
-            // Things to be done while execution of long running operation is in
-            // progress. For example updating ProgessDialog
-        }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                pdLoading.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/gaurav/railslogin/LoginData.java
@@ -0,0 +1,27 @@
+package com.example.gaurav.railslogin;
+
+/**
+ * Created by suchi on 4/12/16.
+ */
+
+public class LoginData {
+    public String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String message;
+}
